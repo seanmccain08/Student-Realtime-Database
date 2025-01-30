2f50530ec4ccff9b23c63c3b2b37aaf15f33cc4d
seanmccain08 <seanmccain327@gmail.com>
2025-01-30 07:37:56 -0600

    Initial Commit

---
+++ Student Realtime Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Realtime Database
+//
+//  Created by SEAN MCCAIN on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Student Realtime DatabaseTests/Student_Realtime_DatabaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Student_Realtime_DatabaseTests.swift
+//  Student Realtime DatabaseTests
+//
+//  Created by SEAN MCCAIN on 1/30/25.
+//
+
+import Testing
+@testable import Student_Realtime_Database
+
+struct Student_Realtime_DatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
